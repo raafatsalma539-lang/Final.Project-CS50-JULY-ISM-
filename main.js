06f548c9cd51c59e540111505ede1ad6f49b2f70
raafatsalma539-lang <raafatsalma539@gmail.com>
2026-08-19 14:21:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggleEvent(id) {
+  var details = document.getElementById(id);
+
+  if (details.style.display === "block") {
+    details.style.display = "none";
+  } else {
+    details.style.display = "block";
+  }
+}
+
+
